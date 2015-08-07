3be9a5bab0c1e0db47bdd2b5df640a8c4175cd63
phillc <spyyderz@gmail.com>
2015-08-07 07:38:13 -0400

    lein new app stmf-ff

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stmf-ff
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/stmf_ff/core.clj
@@ -0,0 +1,7 @@
+(ns stmf-ff.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
