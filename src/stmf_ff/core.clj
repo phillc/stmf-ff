7a8c33a8282b7e7f925254be3ac267aec4eee2cf
phillc <spyyderz@gmail.com>
2015-08-10 21:06:52 -0400

    Create round robin

--- src/stmf_ff/core.clj
+++ src/stmf_ff/core.clj
@@ -18,97 +18,99 @@
    "Annie"
    "Brian"])
 
-;; (def set-schedule
-;;   [[11 ["Phillip" "Kris"]]
-;;    [11 ["Paul" "Mike"]]
-;;    [11 ["Dave" "Brad"]]
-;;    [11 ["Jeff" "Stewart"]]
-;;    [11 ["Angie" "Brian"]]
-;;    [11 ["John" "Annie"]]
-;;    [11 ["Ian" "Drew"]]
-;;    [12 ["Phillip" "Angie"]]
-;;    [12 ["Paul" "Stewart"]]
-;;    [12 ["Dave" "Annie"]]
-;;    [12 ["Jeff" "John"]]
-;;    [12 ["Ian" "Brad"]]
-;;    [12 ["Kris" "Brian"]]
-;;    [12 ["Drew" "Mike"]]])
 
-(def set-schedule
-  (merge
-   (into (sorted-map) (map (fn [week] [week []]) (range 1 14)))
-   {11 [#{"Phillip" "Kris"}
-        #{"Paul" "Mike"}
-        #{"Dave" "Brad"}
-        #{"Jeff" "Stewart"}
-        #{"Angie" "Brian"}
-        #{"John" "Annie"} ;;
-        #{"Ian" "Drew"}]
-    12 [#{"Phillip" "Angie"}
-        #{"Paul" "Stewart"}
-        #{"Dave" "Annie"}
-        #{"Jeff" "John"}
-        #{"Ian" "Brad"}
-        #{"Kris" "Brian"} ;;
-        #{"Drew" "Mike"}]}))
+(def families
+  [#{"Phillip" "Angie"}
+   #{"Paul" "Stewart"}
+   #{"Dave" "Annie"}
+   #{"Jeff" "John"}
+   #{"Drew" "Mike"}])
 
+(def rivals
+  [#{"Phillip" "Angie"}
+   #{"Phillip" "Annie"}
+   #{"Phillip" "Brian"}
+   #{"Phillip" "Dave"}
+   #{"Phillip" "Drew"}
+   #{"Phillip" "Ian"}
+   #{"Phillip" "Kris"}
+   #{"Paul" "Drew"}
+   #{"Paul" "Jeff"}
+   #{"Paul" "Mike"}
+   #{"Paul" "Stewart"}
+   #{"Dave" "Angie"}
+   #{"Dave" "Annie"}
+   #{"Dave" "Brad"}
+   #{"Dave" "Ian"}
+   #{"Jeff" "Drew"}
+   #{"Jeff" "John"}
+   #{"Jeff" "Stewart"}
+   #{"Angie" "Annie"}
+   #{"Angie" "Brian"}
+   #{"Angie" "Drew"}
+   #{"Angie" "Ian"}
+   #{"Ian" "Annie"}
+   #{"Ian" "Brad"}
+   #{"Ian" "Drew"}
+   #{"Brad" "Annie"}
+   #{"Stewart" "Drew"}
+   #{"Drew" "Mike"}
+   ])
 
-(defn already-exists? [players matchups]
-  (some #(some players %) matchups))
+;; (def family-week 12)
 
-(defn can-fit? [matchup [_ week-matchups]]
-  (not (already-exists? matchup week-matchups)))
+;; (def blank-schedule
+;; (into (sorted-map) (map (fn [week] [week []]) (range 1 14))))
 
-(defn find-weeks-for [matchup schedule]
-  (for [[week-number _ :as week] schedule :when (can-fit? matchup week)] week-number))
+;; (defn find-schedules
+;; rotate seeds (In manager order)
+;; satisfies family week
+;; satisfies everyone has a rivlary in week 11 (or 10?)
+;; )
 
-(defn schedule-matchups [needed-matchups current-schedule]
-  ;; first week not full
-  ;; for each remaining matchup
-  ;;   if not already scheduled this week
-  ;;     place in schedule
-  ;;     schedule matchups (other remaining-matchups new-schedule)
+(def league-weeks 13)
 
+;; (defn create-round [size remaining-weeks]
+  ;; first-half)
+;; )
 
+(defn rotate [coll]
+  (take (count coll) (drop (dec (count coll)) (cycle coll))))
 
-  ;; first matchup
-  ;; first week it can go in
-
-  ;; (if (> (count needed-matchups) 75)
-  ;; (println "needed>" (count needed-matchups)))
-
-  (if-let [needed-matchup (first needed-matchups)]
-    ;; (if-let [fit-week (first (filter (partial can-fit-matchup? needed-matchup) current-schedule))]
-    ;; (if-let [fit-week (some (partial can-fit-matchup? needed-matchup) current-schedule)]
-    ;; (if-let [fit-week (first (keep (partial can-fit-matchup? needed-matchup) current-schedule))]
-    ;; (if-let [fit-week (some (partial can-fit-matchup? needed-matchup) current-schedule)]
-    ;; (if-let [fit-week (first (for [[week-number week-matchups] current-schedule :when ()] week-number))]
-    ;; (if-let [weeks (find-weeks-for needed-matchup current-schedule)]
-    (first
-     (let [weeks (find-weeks-for needed-matchup current-schedule)]
-       (for [week weeks
-             :let [schedule (schedule-matchups (rest needed-matchups) (update current-schedule week #(conj % needed-matchup)))]
-             :when schedule]
-         (do (println ">>") schedule))))
-    ;; (schedule-matchups (rest needed-matchups) (update current-schedule fit-week #(conj % needed-matchup)))
-    current-schedule
+(defn round-robin-schedule [size rounds]
+  (loop [seeds (range size)
+         remaining-rounds rounds
+         result []]
+    (if (zero? remaining-rounds)
+      result
+      (let [first-half (take (/ size 2) seeds)
+            second-half (reverse (drop (/ size 2) seeds))]
+            (recur
+             (concat [(first seeds)] (rotate (rest seeds)))
+             (dec remaining-rounds)
+             (conj result (map vector first-half second-half))
+             )))))
 
 (defn -main
   [& args]
-  (let [all-matchups (map set (combinatorics/combinations managers 2))
-        existing-matchups (reduce concat (map second set-schedule))
-        remaining-matchups (difference (set all-matchups) (set existing-matchups))]
-    (println "all-matchups" (count all-matchups) ":" all-matchups)
-    (println "****")
-    (println "existing matchups" (count existing-matchups) ":" existing-matchups)
-    (println "****")
-    (println "matchups remaining" (count remaining-matchups) ":" remaining-matchups)
-    (println "****")
-    (println "set schedule:" set-schedule)
-    (println "****")
-    (let [new-schedule (schedule-matchups remaining-matchups set-schedule)
-          scheduled-matchups (reduce concat (map second new-schedule))
-          unscheduled-matchups (difference (set all-matchups) (set scheduled-matchups))]
-      (println "new schedule:" new-schedule)
-      (println "matchups not scheduled:" (count unscheduled-matchups) unscheduled-matchups)))
+  ;; check all rivalries and families are in managers (check typos)
+
+  (let [schedule (round-robin-schedule (count managers) league-weeks)]
+    (println "round-robin-schedule" schedule))
   )
+;; (let [all-matchups (map set (combinatorics/combinations managers 2))
+;;       existing-matchups (reduce concat (map second set-schedule))
+;;       remaining-matchups (difference (set all-matchups) (set existing-matchups))]
+;;   (println "all-matchups" (count all-matchups) ":" all-matchups)
+;;   (println "****")
+;;   (println "existing matchups" (count existing-matchups) ":" existing-matchups)
+;;   (println "****")
+;;   (println "matchups remaining" (count remaining-matchups) ":" remaining-matchups)
+;;   (println "****")
+;;   (println "set schedule:" set-schedule)
+;;   (println "****")
+;;   (let [new-schedule (schedule-matchups remaining-matchups set-schedule)
+;;         scheduled-matchups (reduce concat (map second new-schedule))
+;;         unscheduled-matchups (difference (set all-matchups) (set scheduled-matchups))]
+;;     (println "new schedule:" new-schedule)
+;;     (println "matchups not scheduled:" (count unscheduled-matchups) unscheduled-matchups)))
